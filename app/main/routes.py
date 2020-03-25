e068a5bc18ed2db2aa3f43ba85c9e49cc35d9247
MediocreMarine <kyle.egland@outlook.com>
2020-03-25 06:25:14 -0500

    Adding about and form with redirect

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,5 +5,11 @@ from app.main import bp
 @bp.route('/')
 @bp.route('/index')
 def index():
-    # Render index
+    # Render index page
     return render_template('index.html')
+
+
+@bp.route('/about')
+def about():
+    # Render about page
+    return render_template('about.html')
